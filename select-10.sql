c30f8186b8e969b56838900e7bafd1d0f3f87706
Taras55001 <taras5500@gmail.com>
2023-06-18 11:36:56 +0200

    modified

--- select-10.sql
+++ select-10.sql
@@ -3,4 +3,5 @@ FROM students AS s
 JOIN grades AS g ON s.id = g.student_id
 JOIN subjects AS sub ON g.subject_id = sub.id
 JOIN teachers AS t ON sub.teacher_id = t.id
-WHERE s.id = 40 AND t.id = 2;
+WHERE s.id = 5 AND t.id = 2
+GROUP BY sub.id;
--- select-8.sql
+++ select-8.sql
@@ -2,4 +2,5 @@ SELECT t.fullname, ROUND(AVG(g.grade), 2), sub.name AS average_grade
 FROM teachers AS t
 JOIN subjects AS sub ON t.id = sub.teacher_id
 JOIN grades AS g ON sub.id = g.subject_id
-GROUP BY t.id;
+WHERE t.id = 2
+GROUP BY sub.id;
